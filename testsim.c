a3a174e6622edeadb9c1ebcbbfe1605d8c0a313b
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-02-04 15:54:12 -0600

    Initial commit of testing program testsim.c

---
+++ testsim.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main (int argc, char *argv[]){
+	int sleepTimer = atoi(argv[1]);
+	int repeatFactor = atoi(argv[2]);
+	
+	int index;
+
+for (index = 0; index <= repeatFactor; index++){
+	sleep(sleepTimer);
+	fprintf (stderr, "Process ID is %d\n", getpid());
+}
+
+return 0;
+
+}
+
+
